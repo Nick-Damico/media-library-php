622b87096604d85784dc6f89fd147b808b010022
Nick-Damico <nickalan82@icloud.com>
2018-04-02 13:25:11 -0400

    add includes inc/functions, inc/data.php, add foreach loop and call to #get_item_html

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 $page_title = 'Personal Media Library';
 $section = Null;
-
+include("inc/data.php");
+include("inc/functions.php");
 include("inc/header.php");
 ?>
 <!DOCTYPE html>
@@ -13,7 +14,11 @@ include("inc/header.php");
 				<h2>May we suggest something?</h2>
 
 				<ul class="items">
-					<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>
+					<?php
+		        foreach($catalog as $id => $item) {
+		          echo get_item_html($id, $item);
+		        }
+		      ?>
 				</ul>
 
 			</div>
