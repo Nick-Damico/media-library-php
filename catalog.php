e7503eda66735a24a145e7a3f61f5919b3861090
Nick-Damico <nickalan82@icloud.com>
2018-04-01 21:39:49 -0400

    add conditional to dynamically set title's based on the category a user requests, and defaults to a not found msg on an unknown source

--- catalog.php
+++ catalog.php
@@ -1,7 +1,21 @@
-<?php include("inc/header.php"); ?>
+<?php
+$page_title = "Full Catalog";
+if (isset($_GET["cat"])) {
+  if ($_GET["cat"] == 'books') {
+    $page_title = 'Books';
+  } else if ($_GET["cat"] == "movies") {
+    $page_title = 'Movies';
+  } else if ($_GET["cat"] == "music") {
+    $page_title = "Music";
+  }
+} else {
+  $page_title = "Opps, this page doesn't exist.";
+}
+include("inc/header.php");
+?>
 
 <div class="section page">
-  <h1>Full Catalog</h1>
+  <h1><?php echo $page_title; ?></h1>
 </div>
 
 <?php include("inc/footer.php"); ?>
