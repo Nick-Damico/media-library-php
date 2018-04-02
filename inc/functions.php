036504d94545aa5cc7f18ebe7160cb2c7df53c1c
Nick-Damico <nickalan82@icloud.com>
2018-04-02 14:30:28 -0400

    array_category(, ) udpated to sort return keys of the catalog items, if category isn't set, then all item id's will be sorted and returned, this is for 'full catalog' page

--- inc/functions.php
+++ inc/functions.php
@@ -7,3 +7,19 @@ function get_item_html($id, $item) {
         . "</a></li>";
  return $output;
 }
+
+function array_category($catalog, $category) {
+  $output = array();
+  foreach($catalog as $id => $item) {
+    if ($category == null || strtolower($item["category"]) == strtolower($category) ) {
+      $sort = $item["title"];
+      $sort = ltrim($sort, "The ");
+      $sort = ltrim($sort, "A ");
+      $sort = ltrim($sort, "And ");
+      $output[$id] = $sort;
+    }
+  }
+
+  asort($output);
+  return array_keys($output);
+}
