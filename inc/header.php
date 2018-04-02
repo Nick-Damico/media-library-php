eb711ee23ce83456c9feb6aed7f57805fd7b8b54
Nick-Damico <nickalan82@icloud.com>
2018-04-01 20:56:08 -0400

    add variable  for a dynamic title on loaded page

--- inc/header.php
+++ inc/header.php
@@ -16,11 +16,11 @@
 			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
 
 			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
+          <li class="books"><a href="catalog.php?cat=books">Books</a></li>
+          <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
+          <li class="music"><a href="catalog.php?cat=music">Music</a></li>
+          <li class="suggest"><a href="suggest.php">Suggest</a></li>
+      </ul>
 
 		</div>
 
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-
 $page_title = 'Personal Media Library';
+include("inc/header.php");
 ?>
-<?php include("inc/header.php"); ?>
 <!DOCTYPE html>
 
 		<div class="section catalog random">
@@ -11,7 +10,7 @@ $page_title = 'Personal Media Library';
 
 				<h2>May we suggest something?</h2>
 
-								<ul class="items">
+				<ul class="items">
 					<li><a href="details.php?id=201"><img src="img/media/forest_gump.jpg" alt="Forrest Gump"><p>View Details</p></a></li><li><a href="details.php?id=204"><img src="img/media/princess_bride.jpg" alt="The Princess Bride"><p>View Details</p></a></li><li><a href="details.php?id=302"><img src="img/media/elvis_presley.jpg" alt="Elvis Forever"><p>View Details</p></a></li><li><a href="details.php?id=303"><img src="img/media/garth_brooks.jpg" alt="No Fences"><p>View Details</p></a></li>
 				</ul>
 
