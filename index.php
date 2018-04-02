ba16af4ac731236332f99df5f5c6f4911a65a74b
Nick-Damico <nickalan82@icloud.com>
2018-04-01 20:31:49 -0400

    modify index.html -> index.php, add suggest.php, add include statements to index.php and suggest.php for re-occuring elements (header.php)

--- index.php
+++ index.php
@@ -2,34 +2,9 @@
 $current_year = Date("Y");
 $page_title = 'Personal Media Library';
 ?>
+<?php include("inc/header.php"); ?>
+<!DOCTYPE html>
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title><?php echo $page_title; ?></title>
-	<link rel="stylesheet" href="css/style.css" type="text/css">
-</head>
-<body>
-
-	<div class="header">
-
-		<div class="wrapper">
-
-			<h1 class="branding-title"><a href="./">Personal Media Library</a></h1>
-
-			<ul class="nav">
-                <li class="books"><a href="catalog.php?cat=books">Books</a></li>
-                <li class="movies"><a href="catalog.php?cat=movies">Movies</a></li>
-                <li class="music"><a href="catalog.php?cat=music">Music</a></li>
-                <li class="suggest"><a href="suggest.php">Suggest</a></li>
-            </ul>
-
-		</div>
-
-	</div>
-
-	<div id="content">
 		<div class="section catalog random">
 
 			<div class="wrapper">
@@ -44,7 +19,7 @@ $page_title = 'Personal Media Library';
 
 		</div>
 
-	</div>
+	</div><!-- end content -->
 
 	<div class="footer">
 
