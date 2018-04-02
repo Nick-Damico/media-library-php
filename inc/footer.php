04a1af6a671b3d3c43d51724d9a6781097806f73
Nick-Damico <nickalan82@icloud.com>
2018-04-01 20:44:12 -0400

    reusable partial file created

---
+++ inc/footer.php
@@ -0,0 +1,20 @@
+<?php $current_year = Date("Y"); ?>
+</div><!-- end content -->
+
+<div class="footer">
+
+  <div class="wrapper">
+
+    <ul>
+      <li><a href="http://twitter.com/treehouse">Twitter</a></li>
+      <li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
+    </ul>
+
+    <p>&copy;<?php echo $current_year; ?> Personal Media Library</p>
+
+  </div>
+
+</div>
+
+</body>
+</html>
