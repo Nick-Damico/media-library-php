5146f53c2b3947198431aa624194f77e7909c98e
Nick-Damico <nickalan82@icloud.com>
2018-04-03 11:23:09 -0400

    suggest.php setup to use PHPMailer library for sending test emails in development using gmail

--- suggest.php
+++ suggest.php
@@ -1,17 +1,72 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+require 'vendor/PHPMailer/src/PHPMailer.php';
+require 'vendor/PHPMailer/src/Exception.php';
+require 'vendor/PHPMailer/src/SMTP.php';
+
 // Form Submission Processing.
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-  $name    = $_POST["name"];
-  $email   = $_POST["email"];
-  $details = $_POST["details"];
+  $name    = trim( filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING) );
+  $email   = trim( filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL) );
+  $details = trim( filter_input(INPUT_POST, "details", FILTER_SANITIZE_SPECIAL_CHARS) );
+
+  // Form Validations
+  if ($name == '' || $email == "" || $details == "") {
+    echo "Please fill in the required fields: Name, Email, and Details";
+    exit;
+  }
+  if ($_POST["address"] != '') {
+    echo "Bad form input";
+    exit;
+  }
+  if (!PHPMailer::validateAddress($email)) {
+    echo "Invalid Email Address";
+    exit;
+  }
 
   $email_body = "";
   $email_body .= "Name "    . $name     . "\n";
   $email_body .= "Email "   . $email    . "\n";
   $email_body .= "Details " . $details  . "\n";
   // To Do: Send email
+
+  $mail = new PHPMailer(true);
+  //Tell PHPMailer to use SMTP
+  $mail->isSMTP();
+  //Enable SMTP debugging
+  // 0 = off (for production use)
+  // 1 = client messages
+  // 2 = client and server messages
+  $mail->SMTPDebug = 2;
+  //Set the hostname of the mail server
+  $mail->Host = 'smtp.gmail.com';
+  // use
+  // $mail->Host = gethostbyname('smtp.gmail.com');
+  // if your network does not support SMTP over IPv6
+  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+  $mail->Port = 587;
+  //Set the encryption system to use - ssl (deprecated) or tls
+  $mail->SMTPSecure = 'tls';
+  //Whether to use SMTP authentication
+  $mail->SMTPAuth = true;
+  //Username to use for SMTP authentication - use full email address for gmail
+  $mail->Username = "nickd28@gmail.com";
+  //Password to use for SMTP authentication
+  $mail->Password = "";                        // Passing `true` enables exceptions
+  //Recipients
+  $mail->setFrom('nickalan82@icloud.com', $name);
+  $mail->addReplyTo($email, $name);
+  $mail->addAddress('nickd28@gmail.com', $name);     // Add a recipient
+  //Content
+  // $mail->isHTML(true);                                  // Set email format to HTML
+  $mail->Subject = 'Media Library Suggestion: ' . $name;
+  $mail->Body    = $email_body;
+  // Send
+  if (!$mail->send()) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    exit;
+  }
   header("location:suggest.php?status=thanks");
-  exit;
 }
 
 // Suggest Page Variables
@@ -47,11 +102,16 @@ include("inc/header.php");
                 <th><label for="Suggest Item Details">Suggestion</label></th>
                 <td><textarea id="details" name="details"></textarea></td>
             </tr>
+            <tr style="display:none">
+                <th><label for="address">Address</label></th>
+                <td><textarea id="address" name="address"></textarea></td>
+                <p>Please leave this input blank on submission.</p>
+            </tr>
           </table>
             <input type="submit" value="Send" />
         </form>
       <?php } ?>
-      
+
   </div>
 
 </div>
