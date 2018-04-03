42b071952cd63a2caf64b7bc3da4696bba95523d
Nick-Damico <nickalan82@icloud.com>
2018-04-02 22:30:59 -0400

    suggest.php now handles append form to page, on submission handles processing of form parameters and redirect back to the suggest.php page with a 'get' request, a query string and parameter of status=thanks with display a thank you message

--- suggest.php
+++ suggest.php
@@ -1,6 +1,25 @@
 <?php
+// Form Submission Processing.
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+  $name    = $_POST["name"];
+  $email   = $_POST["email"];
+  $details = $_POST["details"];
+
+  $email_body = "";
+  $email_body .= "Name "    . $name     . "\n";
+  $email_body .= "Email "   . $email    . "\n";
+  $email_body .= "Details " . $details  . "\n";
+  // To Do: Send email
+  header("location:suggest.php?status=thanks");
+  exit;
+}
+
+// Suggest Page Variables
 $page_title = "Suggest a Media Item";
 $section = "suggest";
+if (isset($_GET["status"]) && $_GET["status"] == 'thanks') {
+  $page_title = "Thank You";
+}
 include("inc/header.php");
 ?>
 
@@ -8,25 +27,31 @@ include("inc/header.php");
 
   <div class="wrapper">
 
-    <h1>Suggest a Media Item</h1>
-    <p>Something you would like to add? Let me know by completing the form below.</p>
-    <form method="post" action="process.php">
-      <table>
-        <tr>
-            <th><label for="name">Name</label></th>
-            <td> <input id="name" type="text" name="name" /></td>
-        </tr>
-        <tr>
-            <th><label for="email">Email</label></th>
-            <td> <input id="email" type="email" name="email" /></td>
-        </tr>
-        <tr>
-            <th><label for="Suggest Item Details">Email</label></th>
-            <td><textarea id="details" name="details"></textarea></td>
-        </tr>
-      </table>
-        <input type="submit" value="Send" />
-    </form>
+    <h1><?php echo $page_title; ?></h1>
+    <?php
+      if (isset($_GET["status"]) && $_GET["status"] == 'thanks') {
+        echo "<p>Thanks for the email! I&rsquo;ll check out your suggestion shortly!</p>";
+      } else { ?>
+        <p>Something you would like to add? Let me know by completing the form below.</p>
+        <form method="post" action="suggest.php">
+          <table>
+            <tr>
+                <th><label for="name">Name</label></th>
+                <td> <input id="name" type="text" name="name" /></td>
+            </tr>
+            <tr>
+                <th><label for="email">Email</label></th>
+                <td> <input id="email" type="email" name="email" /></td>
+            </tr>
+            <tr>
+                <th><label for="Suggest Item Details">Suggestion</label></th>
+                <td><textarea id="details" name="details"></textarea></td>
+            </tr>
+          </table>
+            <input type="submit" value="Send" />
+        </form>
+      <?php } ?>
+      
   </div>
 
 </div>
